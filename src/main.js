9b515360a83d6bf6e63813399363e7fc5ca8dfb9
kerinlin <18296613547@qq.com>
2021-02-23 17:13:44 +0800

    Change eslint config

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/tailwindcss.css'
-import 'normalize.css'
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/tailwindcss.css';
+import 'normalize.css';
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
